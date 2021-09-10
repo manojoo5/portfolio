03406ffd848d570685488248152a5426bb248d38
Manoj Roy <32264503+manojoo5@users.noreply.github.com>
2021-09-10 19:03:58 +0530

    ScrollBar

---
+++ components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from '../../styles/Header.module.css'
+
+export default function Header(){
+    return(
+        <div>
+            <div className={styles.Header}>
+                <div>
+                    Manoj Kumar
+                </div>
+                <div>
+                    KUMAR.MANOJ05@OUTLOOK.COM
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import Header from '../components/layout/Header';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Header/>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default MyApp
