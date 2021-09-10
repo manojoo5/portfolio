917c53e87b4475636b64f71234f06bf0cbe9263e
Manoj Roy <32264503+manojoo5@users.noreply.github.com>
2021-09-10 21:08:30 +0530

    first on dev

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from '../../styles/Header.module.css'
+import HomeIcon from '@material-ui/icons/Home';
+import MailIcon from '@material-ui/icons/Mail';
+
+function Home(){
+
+}
 
 export default function Header(){
     return(
         <div>
             <div className={styles.Header}>
-                <div>
-                    Manoj Kumar
+                <div id={styles.Icon}>
+                    <HomeIcon/>
+                    MANOJ KUMAR
                 </div>
-                <div>
+                <div id={styles.Icon}>
+                    <MailIcon/>
                     KUMAR.MANOJ05@OUTLOOK.COM
                 </div>
             </div>
